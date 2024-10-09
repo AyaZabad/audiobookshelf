77cc513f537a6588529c9386eb1f69d3f5afd3a3
Aya Zabad <az26@students.waikato.ac.nz>
2024-10-10 08:23:54 +1300

    Breadcrumb active crumb when page loads removed query params

--- client/components/ui/Breadcrumb.vue
+++ client/components/ui/Breadcrumb.vue
@@ -1,7 +1,7 @@
 <template>
   <ol class="flex items-center whitespace-nowrap">
     <li v-for="crumb in crumbs" class="inline-flex items-center">
-      <NuxtLink exact-active-class="text-gray-900" :to="`/library/${currentLibraryId}/series/${crumb.id}?filter=series.YzUwMGQxNWYtODQ4ZC00ZjFjLWIyYjctNGExMTcxYzEyYzk5`"
+      <NuxtLink active-class="text-gray-900" :to="`/library/${currentLibraryId}/series/${crumb.id}`"
       >{{crumb.name}}</NuxtLink>
       <svg class="shrink-0 mx-2 size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="m9 18 6-6-6-6"></path>
